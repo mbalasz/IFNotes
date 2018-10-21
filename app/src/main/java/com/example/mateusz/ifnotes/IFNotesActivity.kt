bd0b0ab66c903a95680e71ad0a5ab70008e5fc48
Mateusz Balasz <matti93@gmail.com>
2018-10-21 20:28:39 +0100

    convert to kotlin.

---
+++ app/src/main/java/com/example/mateusz/ifnotes/IFNotesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mateusz.ifnotes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IFNotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ifnotes)
+    }
+}
