a940721bd86d1ecca8b1e0f7194d128b56043f57
Mateusz Balasz <matti93@gmail.com>
2018-10-21 20:26:21 +0100

    Initial view layout.

---
+++ app/src/main/java/com/example/mateusz/ifnotes/IFNotesActivity.java
@@ -0,0 +1,13 @@
+package com.example.mateusz.ifnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IFNotesActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_ifnotes);
+  }
+}
